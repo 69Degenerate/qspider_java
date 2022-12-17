639632f1dd5b02391105a1a848ba1a07e9df7527
69Degenerate <94638388+69Degenerate@users.noreply.github.com>
2022-12-17 16:25:55 +0530

    changre

--- loops/nested loop/pattern 2/l1.java
+++ loops/nested loop/pattern 2/l1.java
@@ -2,12 +2,14 @@ public class l1 {
     public static void main(String[] args) {
         int a=0;
         for (int i = 1; i <= 5; i++) {
+            int b=i+a-1
             for (int j = 1; j <= i; j++) {
                 if (i%2==0){
-                    System.out.print(++a);
+                    System.out.print(a++);
                 }
                 else{
-                    System.out.print(i+a--);
+                    System.out.print(b--);
+                    a++;
                 }
             }
             System.out.println();
