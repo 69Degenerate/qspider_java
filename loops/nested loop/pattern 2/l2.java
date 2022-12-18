59e3ea9b5a28965fb3e0fec777ca58efdcdd5a23
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-12-18 21:11:11 +0530

    ut

--- loops/nested loop/pattern 2/l2.java
+++ loops/nested loop/pattern 2/l2.java
@@ -1,15 +1,12 @@
-public class l1 {
+public class l2 {
     public static void main(String[] args) {
-        int a=0;
         for (int i = 1; i <= 5; i++) {
-            int b=i+a-1;
             for (int j = 1; j <= i; j++) {
-                if (i%2==0){
-                    System.out.print(a++);
+                if (j==1||j==i||i==5){
+                    System.out.print("*");
                 }
                 else{
-                    System.out.print(b--);
-                    a++;
+                    System.out.print(" ");
                 }
             }
             System.out.println();
