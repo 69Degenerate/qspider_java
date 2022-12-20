d4bc17bc81d3958642b2254d20843e6dd26b8f8e
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-12-20 21:24:55 +0530

    25

--- loops/patterns/pattern 2/l10.java
+++ loops/patterns/pattern 2/l10.java
@@ -2,11 +2,10 @@ public class l10 {
     public static void main(String[] args) {
         int a=1;
         for (int i = 1; i <= 5; i++) {
-            int b=a+1;
+            int b=a+i;
             for (int j = 1; j <= i; j++) {
-                System.out.print(i%2==1?a+" ":b+" ");
+                System.out.print(i%2==1?a+" ":--b+" ");
                 a++;
-                b--;
             }
             System.out.println();
         }
