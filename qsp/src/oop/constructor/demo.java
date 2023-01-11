a7aa683ad6059af67ff8fc42663d8e1080fd22c4
69Degenerate <94638388+69Degenerate@users.noreply.github.com>
2023-01-11 12:43:32 +0530

    constr

---
+++ qsp/src/oop/constructor/demo.java
@@ -0,0 +1,12 @@
+class mi{
+    mi(){}
+    mi(int a .int b){
+        System.out.println(a+"\n"+b);
+    }
+}
+class demo{
+    public static void main(String as[]){
+        demo d1=new demo();
+        demo d2=new demo(1,2);
+    }
+}
